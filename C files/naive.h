24bb2852a08401e57ae5336e9e1888db68dde3a9
Knud Ahrens <knud.ahrens@uni-passau.de>
2022-04-01 19:58:06 +0200

    Add new file

---
+++ C files/naive.h
@@ -0,0 +1,9 @@
+#ifndef NAIVE_H
+#define NAIVE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void findSmoothIntsModC (unsigned long start, unsigned int size, unsigned short *smoothNumbers, unsigned short numberRoots, short *roots, unsigned short maxRoot, unsigned short degree, short *poly, unsigned int C, unsigned short *numberSmoothIntsModC, unsigned long *smoothIntsModC, unsigned short maxNumberResults);
+
+#endif
