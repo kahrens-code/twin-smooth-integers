3de0eccccf7c30f45a49b3d9fd23742f7a22bc61
Knud Ahrens <knud.ahrens@uni-passau.de>
2022-04-07 19:23:22 +0200

    Update main.c

--- C files/main.c
+++ C files/main.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
 #include<time.h>
 
@@ -10,18 +9,16 @@
 
 #define COMPARE	//compare different approaches and print running times to screen
 
-///////////////////////////////////// work in progress ////////////////////////////////////////
-
 int main() {
 	
 //general parameters
-	unsigned short maxNumberResults = 100;		//fix the size for the arry of results
-	unsigned int size = pow(2, 20);				//size of chunks
-	unsigned short truncPrime = 1;				//number of small primes to be omitted in the primeTruncLogSieve
-	unsigned short truncPower = 10;				//power of 2 up to which prime powers are omitted in the powerTruncLogSieve
-	unsigned long start = pow(2, 42);			//start of search interval I
-	unsigned int totalSize = 1;		//size of the whole search interval
-	unsigned long end = start + totalSize;		//end of search interval I
+	unsigned short maxNumberResults = 100;	//fix the size for the arry of results
+	unsigned int size = pow(2, 20);		//size of chunks
+	unsigned short truncPrime = 1;		//number of small primes to be omitted in the primeTruncLogSieve
+	unsigned short truncPower = 10;		//power of 2 up to which prime powers are omitted in the powerTruncLogSieve
+	unsigned long start = pow(2, 42);		//start of search interval I
+	unsigned int totalSize = 1;			//size of the whole search interval
+	unsigned long end = start + totalSize;	//end of search interval I
 	unsigned int smoothnessBound = pow(2, 16);	//smoothness bound
 	unsigned long maxSizePrimePower = (unsigned long) smoothnessBound;	//define kind of smoothness:
 		//powersmooth: maxSizePrimePower = smoothnessBound (restrictive)
@@ -29,15 +26,15 @@ int main() {
 		//compromise: e.g. maxSizePrimePower = size (for small smoothness bounds)
 	
 //PTE solution parameters
-	unsigned short degree;			//degree of a(x) and b(x)
-	unsigned short numberRoots;		//can be less than 2*degree if there are double roots
-	short *roots;					//all roots of a(x) and b(x) without multiplicity
-	short *polyA;					//roots of a(x) with multiplicity
-	short *polyB;					//roots of b(x) with multiplicity
-	unsigned int C;					//difference C=|a(x)-b(x)|
+	unsigned short degree;		//degree of a(x) and b(x)
+	unsigned short numberRoots;	//can be less than 2*degree if there are double roots
+	short *roots;			//all roots of a(x) and b(x) without multiplicity
+	short *polyA;			//roots of a(x) with multiplicity
+	short *polyB;			//roots of b(x) with multiplicity
+	unsigned int C;		//difference C=|a(x)-b(x)|
 
 //set PTE specific parameters	
-	unsigned short startParameter = 1;			//0: not changed, >0: set to specific start value
+	unsigned short startParameter = 1;	//0: not changed, >0: set to specific start value
 	setParameters(&n6ex5, &degree, &numberRoots, &roots, &polyA, &polyB, &C, &start, startParameter, totalSize, &end);
 
 //	start = 24924000000000;
@@ -52,29 +49,29 @@ int main() {
 //	end  =  26000000000000;
 
 //variables
-	unsigned long currentStart;			//start of current chunk
+	unsigned long currentStart;		//start of current chunk
 	unsigned int numberSmoothPrimes;	//number of primes below the smoothness bound
-	unsigned int *smoothPrimes;			//pointer to array of such primes
+	unsigned int *smoothPrimes;		//pointer to array of such primes
 	unsigned short *maxExponents;		//highest power of each prime to be included in the search
-	unsigned short *minExponents;			//smallest power of each prime above 2^truncPower
-	unsigned long *logTable;			//array with thresholds for rounded logarithms
+	unsigned short *minExponents;		//smallest power of each prime above 2^truncPower
+	unsigned long *logTable;		//array with thresholds for rounded logarithms
 	unsigned short *logSmoothPrimes;	//rounded logarithms of smooth primes
-	unsigned short tolerance;			//size of allowed non-smooth factor (2^tolerance) for prime truncated log sieve
-	unsigned int surplusSmooth;			//approximate number of additional smooth integers per chunk when using primeTruncLogSieve
+	unsigned short tolerance;		//size of allowed non-smooth factor (2^tolerance) for prime truncated log sieve
+	unsigned int surplusSmooth;		//approximate number of additional smooth integers per chunk when using primeTruncLogSieve
 	#ifdef COMPARE
-	unsigned short tolerance0;			//size of allowed non-smooth factor (2^tolerance) for not truncated log sieve
-	unsigned short tolerancePower;			//size of allowed non-smooth factor (2^tolerance) for power truncated log sieve
-	unsigned int surplusSmooth0;			//approximate number of additional smooth integers per chunk when using logSieve
-	unsigned int surplusSmoothPower;			//approximate number of additional smooth integers per chunk when using powerTruncLogSieve
+	unsigned short tolerance0;		//size of allowed non-smooth factor (2^tolerance) for not truncated log sieve
+	unsigned short tolerancePower;	//size of allowed non-smooth factor (2^tolerance) for power truncated log sieve
+	unsigned int surplusSmooth0;		//approximate number of additional smooth integers per chunk when using logSieve
+	unsigned int surplusSmoothPower;	//approximate number of additional smooth integers per chunk when using powerTruncLogSieve
 	#endif
-	unsigned long *smoothInterval;		//auxiliary array for regular sieve
-	unsigned short *smoothNumbers;		//array of 1s and 0s for smooth and non-smooth integers, respectively
+	unsigned long *smoothInterval;	//auxiliary array for regular sieve
+	unsigned short *smoothNumbers;	//array of 1s and 0s for smooth and non-smooth integers, respectively
 	unsigned int numberResidues;		//number |R| of residue classes r mod C s.t. a(r)/C and b(r)/C are integers
-	unsigned int *residues;				//pointer to array of such classes
-	unsigned int relevantSteps;			//number of "consecutive classes" needed to cover a chunk
-	unsigned short maxRoot;				//largest root of the polynomials a(x) and b(x)
-	unsigned short numberSmoothIntsModC;//number of l in I s.t. a(l)/C and b(l)/C are smooth integers
-	unsigned long *smoothIntsModC;		//pointer to array of such elements
+	unsigned int *residues;		//pointer to array of such classes
+	unsigned int relevantSteps;		//number of "consecutive classes" needed to cover a chunk
+	unsigned short maxRoot;		//largest root of the polynomials a(x) and b(x)
+	unsigned short numberSmoothIntsModC;	//number of l in I s.t. a(l)/C and b(l)/C are smooth integers
+	unsigned long *smoothIntsModC;	//pointer to array of such elements
 
 //timing
 	clock_t initialPre, finalPre;
@@ -90,8 +87,8 @@ int main() {
 //pre-computation
 	preSmoothness(smoothnessBound, &numberSmoothPrimes, &smoothPrimes, maxSizePrimePower, &maxExponents, numberRoots, roots, &maxRoot, &logTable, &logSmoothPrimes, start, size, &smoothInterval, &smoothNumbers, maxNumberResults, &smoothIntsModC, truncPrime, truncPower, &minExponents, &tolerance, &surplusSmooth);
 	#ifdef COMPARE
-	findTolerance (numberSmoothPrimes, smoothPrimes, maxExponents, logTable, logSmoothPrimes, start, size, smoothInterval, smoothNumbers, smoothIntsModC, 0, &tolerance0, &surplusSmooth0);
-	findPowerTolerance (numberSmoothPrimes, smoothPrimes, maxExponents, logTable, logSmoothPrimes, start, size, smoothInterval, smoothNumbers, smoothIntsModC, minExponents, &tolerancePower, &surplusSmoothPower);
+	findTolerance (numberSmoothPrimes, smoothPrimes, maxExponents, logTable, logSmoothPrimes, start, size, smoothInterval, smoothNumbers, 0, &tolerance0, &surplusSmooth0);
+	findPowerTolerance (numberSmoothPrimes, smoothPrimes, maxExponents, logTable, logSmoothPrimes, start, size, smoothInterval, smoothNumbers, minExponents, &tolerancePower, &surplusSmoothPower);
 	#endif
 //pre-computation for new approach
 	initialPre = clock();
@@ -178,7 +175,7 @@ free(minExponents);
 		printf("single Sieve: %.3f ms\n", (double)(finalSieve-initialSieve)/CLOCKS_PER_SEC*1000);
 		printf("single Log Sieve: %.3f ms\n", (double)(finalLogSieve-initialLogSieve)/CLOCKS_PER_SEC*1000);
 		printf("single Trunc Log Sieve: %.3f ms\n", (double)(finalTruncLogSieve-initialTruncLogSieve)/CLOCKS_PER_SEC*1000);
-		printf("single Trunc Log Sieve2: %.3f ms\n", (double)(finalTruncLogSieve2-initialTruncLogSieve2)/CLOCKS_PER_SEC*1000);
+		printf("single Power Trunc Log Sieve: %.3f ms\n", (double)(finalTruncLogSieve2-initialTruncLogSieve2)/CLOCKS_PER_SEC*1000);
 		printf("single Naive: %.3f ms\n", (double)(finalNaive-initialNaive)/CLOCKS_PER_SEC*1000);
 		printf("single New: %.3f ms\n", (double)(finalNew-initialNew)/CLOCKS_PER_SEC*1000);
 		printf("total Naive: %lu s\n", finalNaiveLong-initialNaiveLong);
@@ -188,8 +185,8 @@ free(minExponents);
 	printf("tolerance to include all smooth integers: %u\n", tolerance);
 	printf("roughly %u more smooth integers per chunk\n", surplusSmooth);
 	#ifdef COMPARE
-		printf("not truncated log sieve: tolerance: %u, surplus: %u\n", tolerance0, surplusSmooth0);
-		printf("power truncated log siev: tolerance: %u, surplus: %u\n", tolerancePower, surplusSmoothPower);
+	printf("not truncated log sieve: tolerance: %u, surplus: %u\n", tolerance0, surplusSmooth0);
+	printf("power truncated log siev: tolerance: %u, surplus: %u\n", tolerancePower, surplusSmoothPower);
 	#endif
 	printf("parameters:\n");
 	printf("sBound: %.2f, start: %.2f, end: %.2f, size: %.2f\n", log2(smoothnessBound), log2(start), log2(end), log2(size));
