77c4e07088d2f318f0c8dce311ed7cb22b32716a
Knud Ahrens <knud.ahrens@uni-passau.de>
2022-04-13 16:57:05 +0200

    Upload New File

---
+++ C 128bit/naive.h
@@ -0,0 +1,9 @@
+#ifndef NAIVE_H
+#define NAIVE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void findSmoothIntsModC (unsigned __int128 start, unsigned int size, unsigned short *smoothNumbers, unsigned short numberRoots, short *roots, unsigned short maxRoot, unsigned short degree, short *poly, unsigned int C, unsigned short *numberSmoothIntsModC, unsigned __int128 *smoothIntsModC, unsigned short maxNumberResults);
+
+#endif
